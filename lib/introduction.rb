9382b7286895ef27108f1a650e3a05f4b62f6168
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-16 17:07:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,11 @@ end
 
 introduction_with_language
 
+def introduction_with_language_optional(name = "programmer", language = "Ruby")
+  puts "Hi my name is #{name} and I am learning to program in #{language}"
+end
 
+introduction_with_language_optional
 
 
 
